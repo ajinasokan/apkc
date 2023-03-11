3d9fa7a7f2c57caf643d74eca2a81aac88655a12
Ajin Asokan <ajin.panthayil@gmail.com>
2023-03-11 11:05:47 +0530

    fix: report if bundletool is missing

--- aab.go
+++ aab.go
@@ -6,6 +6,9 @@ import (
 
 func buildAAB() {
 	cmd := exec.Command("bundletool", "build-bundle", "--modules=bundle.zip", "--output=app.aab")
+	if cmd.Err != nil {
+		LogF("build", cmd.Err)
+	}
 	cmd.Dir = "build"
 	out, err := cmd.CombinedOutput()
 	if err != nil {
